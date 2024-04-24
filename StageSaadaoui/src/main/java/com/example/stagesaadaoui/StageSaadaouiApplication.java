093a991d166259306d0e463d6e38d899f45b2b0e
Baganda49 <abdessalemsaa@gmail.com>
2024-04-24 18:11:31 +0200

    initial project spring

---
+++ StageSaadaoui/src/main/java/com/example/stagesaadaoui/StageSaadaouiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stagesaadaoui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageSaadaouiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StageSaadaouiApplication.class, args);
+    }
+
+}
---
+++ StageSaadaoui/src/test/java/com/example/stagesaadaoui/StageSaadaouiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stagesaadaoui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageSaadaouiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
