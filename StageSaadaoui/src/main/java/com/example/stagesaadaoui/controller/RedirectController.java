8af800c273255a38c1532fdbb69db5da5cfc847b
Baganda49 <abdessalemsaa@gmail.com>
2024-06-16 15:07:46 +0200

    add

--- StageSaadaoui/src/main/java/com/example/stagesaadaoui/controller/RedirectController.java
+++ StageSaadaoui/src/main/java/com/example/stagesaadaoui/controller/RedirectController.java
@@ -1,10 +1,14 @@
 package com.example.stagesaadaoui.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/")
 public class RedirectController {
-    @GetMapping("/")
+    @GetMapping("")
     public String redirectToEnfantsList() {
-        return "redirect:enfants/list";
+        return "redirect:/enfants/list";
     }
 }
